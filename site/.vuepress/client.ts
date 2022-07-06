f76d1354118c4b888f09260bdf2db34d9836070c
Lixb53 <li946226@sina.com>
2022-07-06 17:31:47 +0800

    feat: site添加dark样式

--- site/.vuepress/client.ts
+++ site/.vuepress/client.ts
@@ -2,6 +2,7 @@ import { defineClientConfig } from "@vuepress/client";
 // @ts-ignore
 import zzui from "@zzui/zzui";
 import "@zzui/theme-chalk/src/index.scss";
+import "@zzui/theme-chalk/src/dark/css-vars.scss";
 import "./custom.scss";
 
 export default defineClientConfig({
